bb0b48bedd2447bb64b82ab60c3a7ad53579e871
manoj kumar <manojthakur614@gmail.com>
2024-11-27 00:19:10 +0530

    xstates

---
+++ xstates-data/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LocationSelector from './LocationSelector';
+
+function App() {
+  return (
+    <div className="App">
+      <LocationSelector/>
+    </div>
+  );
+}
+
+export default App;
